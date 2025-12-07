2c5c765f78cf235838d221ccf18f30b0254bc753
harbin696969 <harbin309@gmail.com>
2025-12-07 18:11:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+    webpack: (config) => {
+        config.resolve.alias['@'] = __dirname;
+        return config;
+    }
 };
 
 module.exports = nextConfig;
