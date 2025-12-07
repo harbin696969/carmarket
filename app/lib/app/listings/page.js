e3951aaacbe616380790ea25dc4782552c55cc1f
harbin696969 <harbin309@gmail.com>
2025-12-07 17:15:41 +0100

    Update page.js

--- app/lib/app/listings/page.js
+++ app/lib/app/listings/page.js
@@ -1,4 +1,5 @@
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
+
 
 export default async function ListingsPage() {
   // Preberemo oglase iz Supabase
